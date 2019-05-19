14fd581d175381154d8c9b115fa4808f9420da7e
Alexander Rudnev <alexander.rudnev@piano.io>
2019-05-19 14:01:37 +0300

    Добавлены иконки для ингредиентов

--- src/main/java/com/restaurant/entity/Icon.java
+++ src/main/java/com/restaurant/entity/Icon.java
@@ -5,9 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "icon")
 public class Icon implements Data, Serializable {
 
     @Id
